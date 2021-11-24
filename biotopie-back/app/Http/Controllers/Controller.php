419baff8f14c31b0efcca2587afe434dba984fce
kana03 <camille.naulet@hotmail.fr>
2021-11-24 10:39:02 +0100

    relier front et back

--- biotopie-back/app/Http/Controllers/Controller.php
+++ biotopie-back/app/Http/Controllers/Controller.php
@@ -10,4 +10,12 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function handleGetUser(){
+        return response()->json([
+            'name' => 'Abigail',
+            'state' => 'CA',
+        ]);
+
+    }
 }
